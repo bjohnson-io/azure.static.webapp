9d7029f303fb53c50d9d9b7c660c2c9eb4a5ac95
b.johnson <b.johnson@fan-gene.com>
2021-08-18 21:17:53 -0400

    add graphql api

--- api/graphql/index.ts
+++ api/graphql/index.ts
@@ -1,17 +1,6 @@
-import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import { ApolloServer } from 'apollo-server-azure-functions';
+import typeDefs from './schema';
+import resolvers from './resolvers';
 
-const httpTrigger: AzureFunction = async function (ctx: Context, req: HttpRequest): Promise<void> {
-    ctx.log('HTTP trigger function processed a request.');
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-
-    ctx.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
-
-};
-
-export default httpTrigger;
+const server = new ApolloServer({ typeDefs, resolvers });
+export default server.createHandler();
