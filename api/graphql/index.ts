287edb7ddb059a201e43205cf85174b04d99ab36
b.johnson <b.johnson@fan-gene.com>
2021-08-20 20:48:08 -0400

    Revert "add graphql api"
    This reverts commit 9d7029f303fb53c50d9d9b7c660c2c9eb4a5ac95.

--- api/graphql/index.ts
+++ api/graphql/index.ts
@@ -1,6 +1,17 @@
-import { ApolloServer } from 'apollo-server-azure-functions';
-import typeDefs from './schema';
-import resolvers from './resolvers';
+import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 
-const server = new ApolloServer({ typeDefs, resolvers });
-export default server.createHandler();
+const httpTrigger: AzureFunction = async function (ctx: Context, req: HttpRequest): Promise<void> {
+    ctx.log('HTTP trigger function processed a request.');
+    const name = (req.query.name || (req.body && req.body.name));
+    const responseMessage = name
+        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
+        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+
+    ctx.res = {
+        // status: 200, /* Defaults to 200 */
+        body: responseMessage
+    };
+
+};
+
+export default httpTrigger;
